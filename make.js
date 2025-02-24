81f6e2fbbeb3f481941765f1c6980aaea54e8201
Ayush Saini <36878972+imayushsaini@users.noreply.github.com>
2025-02-24 18:59:21 +0530

    ci

--- make.js
+++ make.js
@@ -1,16 +1,19 @@
 var fs = require("fs");
 
 require("dotenv").config({ path: "variables.env" });
-const API = process.env.API;
+const API = process.env.API_PROXY;
+const HOST = process.env.HOST;
 
 //updating vapid key
 data = `export const environment = {
   production: false,
-  API_ENDPOINT: "$API"
+  API_PROXY: "$API",
+  HOST: "$HOST"
 };`;
 dev = "./src/environments/environment.ts";
 prod = "./src/environments/environment.prod.ts";
 var res = data.replace("$API", API);
+var res = data.replace("$HOST", HOST);
 fs.writeFile(dev, res, "utf8", (err) => {
   if (err) console.log(err);
 });
