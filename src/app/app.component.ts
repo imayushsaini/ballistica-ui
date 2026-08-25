02fb5d117a47a58f4a1d093678d13e02d5bd58dc
Ayush Saini <36878972+imayushsaini@users.noreply.github.com>
2026-08-25 18:18:43 +0530

    added gtag

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,8 @@ import { MatDialog } from '@angular/material/dialog';
 import { WelcomeDialogComponent } from './components/welcome-dialog/welcome-dialog.component';
 import { filter } from 'rxjs/operators';
 
+declare const gtag: Function;
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -20,34 +22,38 @@ export class AppComponent implements OnInit {
     private location: Location,
     private dialog: MatDialog,
     private router: Router
-  ) { }
+  ) {}
 
   ngOnInit() {
-    this.router.events.pipe(filter((event) => event instanceof NavigationEnd)).subscribe(() => {
-      const params = this.route.snapshot.queryParams;
-      const host = params['host'];
-      if (host) {
-        this.hostManager.addNewHost(host);
-        if (host !== this.hostManager.getSelectedHost()) {
-          this.hostManager.switchHost(host);
-          // this.refresh();
-        }
-      } else {
-        // if host is default , show dialog to add new server
-        if (this.hostManager.getSelectedHost() === '127.0.0.1:43210') {
-          const dialogRef = this.dialog.open(
-            WelcomeDialogComponent
-          );
-          dialogRef.afterClosed().subscribe((result) => {
-            //route to switch server page
-            this.router.navigate(['/switch-server']);
+    this.router.events
+      .pipe(filter((event): event is NavigationEnd => event instanceof NavigationEnd))
+      .subscribe((event) => {
+        // Send page_view to Google Analytics (gtag)
+        if (typeof gtag === 'function') {
+          gtag('config', 'G-7LMMM98DW5', {
+            page_path: event.urlAfterRedirects,
           });
-
         }
-      }
-    });
 
+        const params = this.route.snapshot.queryParams;
+        const host = params['host'];
+        if (host) {
+          this.hostManager.addNewHost(host);
+          if (host !== this.hostManager.getSelectedHost()) {
+            this.hostManager.switchHost(host);
+          }
+        } else {
+          // if host is default, show dialog to add new server
+          if (this.hostManager.getSelectedHost() === '127.0.0.1:43210') {
+            const dialogRef = this.dialog.open(WelcomeDialogComponent);
+            dialogRef.afterClosed().subscribe(() => {
+              this.router.navigate(['/switch-server']);
+            });
+          }
+        }
+      });
   }
+
   refresh(): void {
     this.location.go(this.location.path());
     location.reload();
